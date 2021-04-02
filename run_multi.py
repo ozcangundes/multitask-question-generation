83afb89a94ac6f8250c5a873513a3b68012fe272
OzcanGundes <ozcangundes@gmail.com>
2021-04-02 13:06:12 +0300

    bug fix

--- run_multi.py
+++ run_multi.py
@@ -195,8 +195,8 @@ def main(args_file=None):
         trainer.save_model()
         # For convenience, we also re-save the tokenizer to the same directory,
         # so that you can share your model easily on huggingface.co/models =)
-        if trainer.is_world_master():
-            tokenizer.save_pretrained(training_args.output_dir)
+        
+        tokenizer.save_pretrained(training_args.output_dir)
 
     # Evaluation
     results = {}
